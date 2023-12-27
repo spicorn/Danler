62b1b72c55f000efde973eaa653c5cd3e1f9b6e8
Dancel Mautsa <70059622+spicorn@users.noreply.github.com>
2023-12-27 09:52:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".hero-container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  hero-container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
